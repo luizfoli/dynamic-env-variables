d098f2b51d48f2fe49e84d29a1bb5aa4e5594692
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-04-12 15:53:45 -0300

    fix: App
    Ajustando o component e parte de view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { ConfigService } from './../config.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  private webConfig;
+  public urlApi: string;
+  public company: string;
+
+
+  constructor(private config: ConfigService) {
+  }
+
+  ngOnInit(): void {
+    this.webConfig = this.config.getWebConfig();
+
+    this.urlApi = this.webConfig.apiUrl;
+    this.company = this.webConfig.company;
+  }
+
+
+
+
   title = 'builded-var';
+
+
+
+
 }
